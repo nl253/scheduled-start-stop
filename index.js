9ce71a987fc51d9cbc27d0f6856ce89ee71d356d
Norbert Logiewa <norbertlogiewa96@gmail.com>
2020-04-16 12:18:59 +0100

    Use AWS tags not SSM

--- index.js
+++ index.js
@@ -1,40 +1,78 @@
-const { EC2, SSM } = require('aws-sdk');
+const { EC2 } = require('aws-sdk');
 const ec2 = new EC2();
-const ssm = new SSM();
+const TAG_NAME = 'scheduled-start-stop';
 
 /**
- * @return {Promise<{HOUR_START: number, HOUR_END: number, INSTANCE_ID: string[], REGION: string}>}
+ * @return {Promise<{tags: Record<string, string>, id: string, state: ("pending"|"running"|"shutting-down"|"terminated"|"stopping"|"stopped")}[]>}
  */
-const getParams = async () => {
-  const last = array => array[array.length - 1];
-  const params = (await ssm.getParametersByPath({
-    Path: process.env.PARAMS_PATH,
-    WithDecryption: true,
-  }).promise()).Parameters;
-  const o = Object.fromEntries(params.map(({Name, Value}) => [last(Name.split('/')), Value]));
-  o.INSTANCE_ID = o.INSTANCE_ID.split(',');
-  o.HOUR_START = parseInt(o.HOUR_START);
-  o.HOUR_END = parseInt(o.HOUR_END);
-  return o;
+const getInstances = async () => {
+  const vms = await ec2.describeInstances({ Filters: [{Name: 'tag-key', Values: [TAG_NAME]}] }).promise();
+  return vms.Reservations
+            .flatMap((({Instances}) => Instances))
+            .flatMap(vms => vms)
+            .map(({ InstanceId, State, Tags }) => ({
+    state: State.Name,
+    id: InstanceId,
+    tags: Object.fromEntries(Tags.map(({ Key, Value }) => [Key, Value])),
+  }));
 };
 
 exports.handler = async (_event, _context) => {
-  const { HOUR_END, INSTANCE_ID, HOUR_START } = await getParams();
-
   const d = new Date();
   const h = d.getUTCHours() + 1;
 
-  console.log(`it's ${d}, hour is ${h}, start hour is ${HOUR_START}, end hour is ${HOUR_END}`);
+  console.log(`it's ${d}, hour is ${h}`);
+
+  const res = {
+    start: { instances: [], info: undefined },
+    stop: { instances: [], info: undefined },
+    ignore: { instances: [] },
+  };
 
   try {
-    let body = 'nothing happened';
-    if (h === HOUR_START) {
-      body = JSON.stringify(await ec2.startInstances({InstanceIds: INSTANCE_ID}).promise());
-    } else if (h === HOUR_END) {
-      body = JSON.stringify(await ec2.stopInstances({InstanceIds: INSTANCE_ID}).promise());
+    const instances = await getInstances();
+    console.info(`collected instances: ${JSON.stringify(instances)}`);
+    for (const {id, tags, state} of instances) {
+      const name = tags.Name ? `${tags.Name} ${id}` : id;
+      const [timeStart, timeEnd] = tags[TAG_NAME].split(/-+/g)
+                                                 .map(s => s.trim())
+                                                 .filter(Boolean)
+                                                 .map(s => parseInt(s));
+      console.info(`time bounds for ${name} are ${timeStart} to ${timeEnd}`);
+      if (h >= timeStart && h <= timeEnd) {
+        if (state === 'stopped') {
+          console.info(`scheduling ${name} for start`);
+          res.start.instances.push(id);
+        } else {
+          console.info(`nothing to be done for ${name}`);
+          res.ignore.instances.push(id);
+        }
+      } else {
+        if (state === 'stopped') {
+          console.info(`nothing to be done for ${name}`);
+          res.ignore.instances.push(id);
+        } else {
+          console.info(`scheduling ${name} for stop`);
+          res.stop.instances.push(id);
+        }
+      }
     }
-    console.log(body);
-    return { body, statusCode: 200 };
+
+    await Promise.all([
+      (async () => {
+        if (res.start.instances.length > 0) {
+          res.start.info = await ec2.startInstances({InstanceIds: res.start.instances}).promise();
+        }
+      })(),
+      (async () => {
+        if (res.stop.instances.length > 0) {
+          res.stop.info = await ec2.stopInstances({InstanceIds: res.stop.instances}).promise();
+        }
+      })(),
+    ]);
+
+    return { body: JSON.stringify(res), statusCode: 200 };
+
   } catch (e) {
     e = JSON.stringify(e);
     console.error(e);
